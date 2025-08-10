9d7cc7240768fef0c69a051743b437b9ec432005
Sri Pragna Bitra <sripragnabitra@gmail.com>
2025-08-10 17:30:25 +0530

    Code for all the models

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -17,13 +17,14 @@ def load_mnist_labels(filename):
         labels = np.frombuffer(f.read(), dtype=np.uint8)
         return labels
 
-def load_mnist():
+def load_mnist(normalise=False):
     X_train = load_mnist_images(os.path.join(DATA_DIR, "train-images.idx3-ubyte"))
     y_train = load_mnist_labels(os.path.join(DATA_DIR, "train-labels.idx1-ubyte"))
     X_test  = load_mnist_images(os.path.join(DATA_DIR, "t10k-images.idx3-ubyte"))
     y_test  = load_mnist_labels(os.path.join(DATA_DIR, "t10k-labels.idx1-ubyte"))
     
-    X_train = X_train / 255.0
-    X_test  = X_test / 255.0
+    if normalise:
+        X_train = X_train / 255.0
+        X_test  = X_test / 255.0
     
     return X_train, y_train, X_test, y_test
